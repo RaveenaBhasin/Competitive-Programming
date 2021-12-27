3eef1aee4c78032b51212bc178a19ff0b8912cb4
Raveena Bhasin <73697080+RaveenaBhasin@users.noreply.github.com>
2021-12-27 10:02:53 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (13.87%) - LeetHub

---
+++ number-complement/number-complement.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        vector<int>v; 
+	    // convert to binary representation
+        while(num != 0){
+            v.push_back(num % 2);
+            num /= 2;
+        } 
+		// make complement
+        for(int i=0; i<v.size(); i++){
+            if(v[i] == 1){
+                v[i] = 0;
+            }
+            else if(v[i] == 0){
+                v[i] = 1;
+            }
+        } 
+        //convert binary to decimal
+        int res = 0;
+        for(int i=v.size()-1; i>-1; i--){
+            res = res * 2 + v[i];
+        }
+        return res;
+    }
+};
