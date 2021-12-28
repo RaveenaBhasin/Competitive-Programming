44eb773c5572c34dea36d0317b5959efaa283551
Raveena Bhasin <73697080+RaveenaBhasin@users.noreply.github.com>
2021-12-28 12:03:16 +0530

    Time: 16 ms (54.58%), Space: 12.1 MB (12.11%) - LeetHub

---
+++ two-sum/two-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>v;
+        unordered_map<int, int>mp;
+        for(int i = 0; i < nums.size(); i++){
+            if(mp.find(target - nums[i])!=mp.end()){
+                v.push_back(mp[target - nums[i]]);
+                v.push_back(i);
+            }
+            mp[nums[i]] = i;
+        }
+        return v;
+    }
+};
