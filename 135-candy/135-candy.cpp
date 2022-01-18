667351278870b50191696de1c4eff833af0a4b95
Raveena Bhasin <73697080+RaveenaBhasin@users.noreply.github.com>
2022-01-18 19:15:35 +0530

    Time: 36 ms (24.10%), Space: 17.7 MB (81.91%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int countCandies(vector<int>&arr, int n){
+        int sum = 0;
+        vector<int>ans(n, 1);
+        for(int i = 0; i < n-1; i++){
+            if(arr[i+1] > arr[i] && ans[i+1] <= ans[i]){
+                ans[i+1] = ans[i] + 1;
+            }
+        }
+        for(int i = n-2; i >= 0; i--){
+            if(arr[i+1] < arr[i] && ans[i+1] >= ans[i]){
+                ans[i] = ans[i+1] + 1;
+            }
+            // else if(arr[i+1] == arr[i] && ans[i+1] > ans[i]){
+            //     ans[i] = ans[i+1] + 1;
+            // }
+            sum += ans[i];
+        }
+        sum += ans[n-1];
+        return sum;
+    }
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        return countCandies(ratings, n);
+    }
+};
