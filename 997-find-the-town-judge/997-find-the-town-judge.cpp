2aec8515a1b45689b4333371e95dda1d4999add7
Raveena Bhasin <73697080+RaveenaBhasin@users.noreply.github.com>
2022-01-21 13:31:08 +0530

    Time: 237 ms (60.38%), Space: 68.9 MB (27.38%) - LeetHub

---
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int>trusted(n+1, 0);
+        for(auto person : trust){
+            trusted[person[0]]--;
+            trusted[person[1]]++;
+        }
+        for(int i = 1; i <= n; i++){
+            if(trusted[i] == n-1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
