314d294ca3f5366b229597ccd20d6038a1b8f113
Raveena Bhasin <73697080+RaveenaBhasin@users.noreply.github.com>
2022-02-18 09:58:03 +0530

    Time: 3 ms (55.05%), Space: 7.8 MB (59.33%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
     int dp[101][2];
-
-    int maxMoney(vector<int>&nums, int currIndex, int n, bool isValid){
-        if(currIndex >= n){
+    
+    int maxMoney(vector<int>&nums, int cI, int n, bool isValid){
+        if(cI >= n){
             return 0;
         }
-        if(dp[currIndex][isValid] != -1){
-            return dp[currIndex][isValid];
+        if(dp[cI][isValid] != -1){
+            return dp[cI][isValid];
         }
         int rob = 0, noRob = 0;
         if(isValid){
-            rob = nums[currIndex] + maxMoney(nums, currIndex + 1, n, false);
+            rob = nums[cI] + maxMoney(nums, cI + 2, n, true);
         }
-        noRob = maxMoney(nums, currIndex + 1, n, true);
-        return dp[currIndex][isValid] = max(rob, noRob);
+        noRob = maxMoney(nums, cI + 1, n, true);
+        return dp[cI][isValid] = max(rob, noRob);
     }
-    
-
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n==1) return nums[0]; 
-		//when n==1, case1 and case2 return 0 but we have to select the house as there are no adjecent house.
+        if(n == 1){
+            return nums[0];
+        }
+        memset(dp, -1, sizeof(dp));
         
-        memset(dp,-1,sizeof(dp));
-		//case 1: rob from 0...n-1, because all houses are in circular so nth is adjecent to 0th house
         int case1 = maxMoney(nums, 0, n-1, true);
-		
-        memset(dp,-1,sizeof(dp));
-		//case 2: rob from 1...n,
-        int case2 = maxMoney(nums,1, n, true);
-		
-		//return maximum between case1 & case2
-        return max(case1,case2);
+        
+        memset(dp, -1, sizeof(dp));
+    
+        int case2 = maxMoney(nums, 1, n, true);
+        
+        return max(case1, case2);
     }
 };
