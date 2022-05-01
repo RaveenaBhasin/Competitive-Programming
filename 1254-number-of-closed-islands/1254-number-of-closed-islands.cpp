6bc478372881931ddb261903a25bc766cd201fb2
Raveena Bhasin <73697080+RaveenaBhasin@users.noreply.github.com>
2022-05-01 16:08:25 +0530

    Time: 13 ms (71.94%), Space: 9.4 MB (66.88%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
+    bool isIsland(vector<vector<int>>&grid, int i, int j, int m, int n){
+        if(i<0 || i>=m || j<0 || j>=n){
+            return false;
+        }
+        if(grid[i][j] == 1){
+            return true;
+        }
+        grid[i][j] = 1;
+        bool d1 = isIsland(grid, i+1, j, m, n);
+        bool d2 = isIsland(grid, i-1, j, m, n);
+        bool d3 = isIsland(grid, i, j+1, m, n);
+        bool d4 = isIsland(grid, i, j-1, m, n);
+        return d1&d2&d3&d4;
+    }
     int closedIsland(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
         int count = 0;
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if((grid[i][j]==0 && sinkIsland(grid, i, j, m, n)) == true){
+                if(grid[i][j] == 0 && isIsland(grid, i, j, m, n)){
                     count++;
                 }
             }
         }
         return count;
     }
-    
-    bool sinkIsland(vector<vector<int>>&grid, int currRow, int currCol, int m, int n){
-        if(currRow < 0 || currRow >= m || currCol < 0 || currCol >= n){
-            return false;
-        }
-        if(grid[currRow][currCol] == 1){
-            return true;
-        }
-        grid[currRow][currCol] = 1;
-        bool a = sinkIsland(grid, currRow - 1, currCol, m, n);
-        bool b = sinkIsland(grid, currRow, currCol - 1, m ,n);
-        bool c = sinkIsland(grid, currRow, currCol + 1, m, n);
-        bool d = sinkIsland(grid, currRow + 1, currCol, m, n);
-        return a&b&c&d;
-    }
-    
-
 };
