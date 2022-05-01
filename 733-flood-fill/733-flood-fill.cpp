2822d9dde2267aaa04ccf2e94de3975fcf981c56
Raveena Bhasin <73697080+RaveenaBhasin@users.noreply.github.com>
2022-05-01 14:22:44 +0530

    Time: 15 ms (41.00%), Space: 14 MB (51.43%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& grid, int sr, int sc, int newColor) {
-        if(grid[sr][sc] == newColor){
-            return grid;
-        }
-        sinkGraph(grid, sr, sc, grid[sr][sc], grid.size(), grid[0].size(), newColor);
-        return grid;
-    }
-    
-    void sinkGraph(vector<vector<int>>&graph, int currRow, int currCol, int startColor, int m, int n, int newColor){
-        if(currRow < 0 || currRow >= m || currCol < 0 || currCol >= n || graph[currRow][currCol] != startColor){
+    void floodFiller(vector<vector<int>>&image, int currRow, int currCol, int m, int n, int currColor, int newColor){
+        if(currRow < 0 || currRow >= m || currCol < 0 || currCol >= n || image[currRow][currCol] == newColor || image[currRow][currCol] != currColor){
             return;
         }
-        graph[currRow][currCol] = newColor;
-        //up direction
-        sinkGraph(graph, currRow - 1, currCol, startColor, m, n, newColor);
-        //right direction
-        sinkGraph(graph, currRow, currCol + 1, startColor, m, n, newColor);
-        //left direction
-        sinkGraph(graph, currRow, currCol - 1, startColor, m, n, newColor);
-        //down direction
-        sinkGraph(graph, currRow + 1, currCol, startColor, m, n, newColor);
-        return;
+        image[currRow][currCol] = newColor;
+        floodFiller(image, currRow-1, currCol, m, n, currColor, newColor);
+        floodFiller(image, currRow+1, currCol, m, n, currColor, newColor);
+        floodFiller(image, currRow, currCol-1, m, n, currColor, newColor);
+        floodFiller(image, currRow, currCol+1, m, n, currColor, newColor);
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        int m = image.size();
+        int n = image[0].size();
+        int currColor = image[sr][sc];
+        floodFiller(image, sr, sc, m, n, currColor, newColor);
+        return image;
     }
 };
