59a25476f68a21b5cf70fb3222ecea8b9c55671f
Raveena Bhasin <73697080+RaveenaBhasin@users.noreply.github.com>
2022-05-01 14:32:58 +0530

    Time: 27 ms (51.80%), Space: 23.1 MB (89.29%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
+    int findArea(vector<vector<int>>&grid, int currRow, int currCol, int m ,int n){
+        if(currRow < 0 || currRow >= m || currCol < 0 || currCol >= n || grid[currRow][currCol] == 0){
+            return 0;
+        }
+        grid[currRow][currCol] = 0;
+        int area = 1;
+        area += findArea(grid, currRow-1, currCol, m, n);
+        area += findArea(grid, currRow+1, currCol, m, n);
+        area += findArea(grid, currRow, currCol-1, m, n);
+        area += findArea(grid, currRow, currCol+1, m, n);
+        return area;
+    }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size(), mx = 0;
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                if (grid[r][c] == 1) {
-                    mx = max(mx, area(grid, r, c, m, n));
+        int m = grid.size();
+        int n = grid[0].size();
+        int maxArea = 0;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 1){
+                    maxArea = max(maxArea, findArea(grid, i, j, m, n));
                 }
             }
         }
-        return mx;
+        return maxArea;
+        
     }
-    
-    int area(vector<vector<int>>& grid, int r, int c, int m, int n) {
-        int x[] = {0, 0, 1, -1};
-        int y[] = {1, -1, 0, 0};
-        int a = 0;
-        grid[r][c] = 0;
-        for (int i = 0; i < 4; i++) {
-            if (r + x[i] >=0 && r + x[i] < m && c + y[i] >=0 && c + y[i] < n && grid[r + x[i]][c + y[i]] == 1) {
-                a += area(grid, r + x[i], c + y[i], m, n); 
-            }
-        }
-        return a + 1;
-    }
-
 };
