12d819947d6ad565674a7c31b8c45b62fc2adaa6
Raveena Bhasin <73697080+RaveenaBhasin@users.noreply.github.com>
2022-05-05 12:55:16 +0530

    Time: 61 ms (84.60%), Space: 21.7 MB (59.54%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -13,15 +13,14 @@ public:
     int numEnclaves(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        int count = 0;
-        //Boundary 1's
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if((i == 0 || j == 0 || i == m-1 || j == n-1) && grid[i][j] == 1){
-                    enclaveFound(grid, i, j, m, n);            
+                if((i == 0|| j == 0 || i == m-1 || j == n-1) && grid[i][j] == 1){
+                    enclaveFound(grid, i, j, m, n);
                 }
             }
         }
+        int count = 0;
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] == 1){
