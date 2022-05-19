1b4daea238ff4ffeb5a7ed346f4db6b956373d61
Raveena Bhasin <73697080+RaveenaBhasin@users.noreply.github.com>
2022-05-20 00:07:50 +0530

    Time: 54 ms (71.93%), Space: 14.8 MB (95.23%) - LeetHub

---
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    int dp[200][200], cnt, m, n;
+    int solve(vector<vector<int>>& mat, int i, int j, int prev){
+        if(i < 0 || j < 0 || i >= n || j >= m || mat[i][j] <= prev){
+            return 0;
+        }
+        if(dp[i][j]){
+            return dp[i][j];
+        }
+        return dp[i][j] = 1 + max({ solve(mat, i + 1, j, mat[i][j]),
+                                    solve(mat, i - 1, j, mat[i][j]),
+                                    solve(mat, i, j + 1, mat[i][j]),
+                                    solve(mat, i, j - 1, mat[i][j]) });       
+    }
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        cnt = 0, n = size(matrix), m = size(matrix[0]);
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                cnt = max(cnt, solve(matrix, i, j, -1));            
+        return cnt;
+    }
+};
+
