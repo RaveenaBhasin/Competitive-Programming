4df888b454e7e87f9a70d71eca0c58e55e671056
Raveena Bhasin <73697080+RaveenaBhasin@users.noreply.github.com>
2022-08-11 23:59:32 +0530

    Time: 6 ms (36.76%), Space: 7.1 MB (94.03%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
-    int splitArray(vector<int>& nums, int m) {
-        int l=0,r=0,n=nums.size();
-        for(int i=0;i<n;++i) l=max(l,nums[i]), r+=nums[i];
-        
-        int mid=0,ans=0;
-        while(l<=r){
-            mid=(l+r)/2;
-            int count=0,tempsum=0;
-            for(int i=0;i<n;++i){
-                if(tempsum+nums[i]<=mid) tempsum+=nums[i];
-                else count++,tempsum=nums[i];
+    int isPossible(vector<int>&nums, int mid) {
+        int count = 1, currSum = 0, n = nums.size();
+        for(int i = 0; i < n; i++) {
+            if(currSum + nums[i] <= mid) {
+                currSum += nums[i];
             }
-            count++; 
-            
-            if(count<=m) r=mid-1, ans=mid;
-            else l=mid+1;
-        }  
+            else{
+                count++;
+                currSum = nums[i];
+            }
+        }
+        return count;
+    }
+    int splitArray(vector<int>& nums, int m) {
+        int low = 0, high = 0;
+        int n = nums.size();
+        for(int i = 0; i < n; i++) {
+            low = max(low, nums[i]);
+            high += nums[i];
+        }
+        int mid = 0, ans = 0;
+        while(low <= high) {
+            mid = low + (high-low)/2;
+            int count = isPossible(nums, mid);  //no. of subarrays possible if max sum is mid
+            if(count <= m){
+                high = mid-1;
+                ans = mid;
+            }
+            else{
+                low = mid+1;
+            }
+        }
         return ans;
     }
 };
