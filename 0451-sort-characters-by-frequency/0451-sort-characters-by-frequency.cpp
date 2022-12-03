41a03a1b5a91796e03e9b050120af820d24597a6
Raveena Bhasin <73697080+RaveenaBhasin@users.noreply.github.com>
2022-12-03 09:35:42 +0530

    Time: 534 ms (5.02%), Space: 8 MB (95.21%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        unordered_map<char, int> mp; 
-        for(auto c : s) {
-            mp[c]++; 
+        unordered_map<char, int> m; // Create a hashmap to store the count of each character in the string
+        for (auto c : s) {
+            m[c]++; // Increment the count of the character c in the hashmap
         }
         sort(s.begin(), s.end(), [&](char a, char b) {
-            return mp[a] > mp[b] || (mp[a] == mp[b] && a < b);
+            // Sort the string in decreasing order of the count of each character in the string.
+            // If the count is same, then sort the characters in increasing order.
+            return m[a] > m[b] || (m[a] == m[b] && a < b);
         });
         return s;
     }
