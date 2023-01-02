965a5afb3a0312eb2adaf104afa16f803febbe23
Raveena Bhasin <73697080+RaveenaBhasin@users.noreply.github.com>
2023-01-02 09:21:05 +0530

    Time: 5 ms (24.37%), Space: 6 MB (89.31%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        if(isupper(word[0]) && isupper(word[1])) {
+            for(int i = 2; i < word.length(); i++) {
+                if(islower(word[i])) return false;
+            }
+        }
+        else {
+            for(int i = 1; i < word.length(); i++) {
+                if(isupper(word[i])) return false;
+            }
+        }
+        return true;
+    }
+};
